462d743d1fb2ac12aa594b344c6ee65f2c5ed2e1
63832 <153898686+jhamalainen0@users.noreply.github.com>
2025-10-06 08:40:56 +0300

    Lagt till delete och förhindra att man kan lägga till samma två gånger

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -8,14 +8,23 @@ use Illuminate\Support\Facades\View;
 
 class TodoController extends Controller {
     function show() {
-       $lista=['Cykla', "Sova", 'Andas', "Äta"];   
+       $lista = ['Cykla', "Sova", 'Andas', "Äta"];   
         
        return View::make('todo', ['lista' => $lista]);
     }
 
     function add(Request $request) {
-        $lista=json_decode($request->request->get('lista'));
+        $lista = json_decode($request->request->get('lista'));
+        if (!in_array($request->request->get('uppgift'),$lista)) {
         $lista[] = $request->request->get('uppgift'); 
+        }
         return View::make('todo', ['lista'=>$lista]);
+    }
+
+    function remove(Request $request) {
+        $lista = json_decode($request->request->get('lista'));
+        $lista = array_values(array_diff($lista, [$request->request->get('uppgift')]));
+
+        return View::make('todo', ['lista' => $lista]);
     }
 }
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,26 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo-lista</title>
 </head>
+
 <body>
     <h1>Todo-lista</h1>
     <form method="POST">
-        Uppgift: <input name="uppgift" placeholder="Skriv in en uppgift" required>
+        Uppgift: <input name="uppgift" placeholder="Skriv in en uppgift" required autocomplete="off">
         <input type="submit" value="Lägg till">
-        <input name="lista" value="{{json_encode($lista)}}" hidden>
+        <input type="hidden" name="lista" value="{{json_encode($lista)}}">
     </form>
     @if (empty($lista))
-        <p>Det finns inget att göra!!!</p>
-    @else 
-        <h2>Uppgifter</h2>
-        <ul>
-            @foreach($lista as $uppgift)
-                <li>{{$uppgift}}</li>
-                @endforeach
-        </ul>
+    <p>Det finns inget att göra!!!</p>
+    @else
+    <h2>Uppgifter</h2>
+    <ul>
+        @foreach($lista as $uppgift)
+        <li>
+            <form method="POST">
+                <input type="hidden" name="lista" value="{{json_encode($lista)}}">
+                {{$uppgift}}
+                <input type="hidden" name="uppgift" value="{{$uppgift}}">
+                <input type="hidden" name="_method" value="DELETE">
+                    <input type="submit" value="Ta bort">
+            </form>
+        </li>
+        @endforeach
+    </ul>
     @endif
 </body>
+
 </html>
