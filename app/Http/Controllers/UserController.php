dc07342be4dbe373b9909010d2e3d431e7110603
63832 <153898686+jhamalainen0@users.noreply.github.com>
2025-10-09 12:28:44 +0300

    Visa alla användare i en lista under formuläret

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,11 +15,13 @@ class UserController extends Controller {
     } 
 
     function show() {
-        return View::make('user');
+        $lista = $this->repo->all();
+        return View::make('user', ['lista'=>$lista]);
     }
 
     function add(Request $request) {
         $user = User::factory()->make($request->request->all());
         $this->repo->add($user);
+        return redirect('/anvandare');
     }
 }
